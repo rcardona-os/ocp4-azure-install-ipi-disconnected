154803eb1a907ceafa6ba1399ec6f3ea2ad17909
rafael.cardona.it@gmail.com <rafael.cardona.it@gmail.com>
2024-11-14 17:39:31 +0100

    done

--- ocp-terraform/06-ocp-worker-nsg.tf
+++ ocp-terraform/06-ocp-worker-nsg.tf
@@ -1,8 +1,8 @@
 # Define the Network Security Group (NSG) for internet access
-resource "azurerm_network_security_group" "ocp-nsg" {
-  name                = "ocp-nsg"
-  location            = azurerm_resource_group.ocp-private-rg.location
-  resource_group_name = azurerm_resource_group.ocp-private-rg.name
+resource "azurerm_network_security_group" "worker_nsg" {
+  name                = "worker_nsg"
+  location            = var.location
+  resource_group_name = var.resource_group_name
 
   # Allow SSH
   security_rule {
