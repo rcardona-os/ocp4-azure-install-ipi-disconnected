eb1ecc6a7dd164d1363a3595497a806329ce9a15
rafael.cardona.it@gmail.com <rafael.cardona.it@gmail.com>
2024-11-14 15:52:15 +0100

    done

--- ocp-terraform/03-vnet-subnet.tf
+++ ocp-terraform/03-vnet-subnet.tf
@@ -1,6 +1,6 @@
 # Define VNet and Subnets
 resource "azurerm_virtual_network" "vnet" {
-  name                = var.vnet_name
+  name                = "ocp_vnet"
   location            = var.location
   resource_group_name = var.resource_group_name
   address_space       = [var.vnet_cidr]
@@ -8,7 +8,7 @@ resource "azurerm_virtual_network" "vnet" {
 
 # Public subnet to host registry
 resource "azurerm_subnet" "public_subnet" {
-  name                 = var.public_subnet_name
+  name                 = "public_subnet"
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [var.public_subnet_cidr]
@@ -16,7 +16,7 @@ resource "azurerm_subnet" "public_subnet" {
 
 # Private subnet masters
 resource "azurerm_subnet" "master_subnet" {
-  name                 = var.master_subnet_name
+  name                 = "master_subnet"
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [var.master_subnet_cidr]
@@ -24,7 +24,7 @@ resource "azurerm_subnet" "master_subnet" {
 
 # Private subnet workers
 resource "azurerm_subnet" "worker_subnet" {
-  name                 = var.worker_subnet_name
+  name                 = "worker_subnet"
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [var.worker_subnet_cidr]
