5c9ebb3e235f2a8d6e6953aeb932e1586536cdba
rafael.cardona.it@gmail.com <rafael.cardona.it@gmail.com>
2024-11-14 23:22:49 +0100

    done

--- ocp-terraform/03-vnet-subnet.tf
+++ ocp-terraform/03-vnet-subnet.tf
@@ -12,6 +12,7 @@ resource "azurerm_subnet" "public_subnet" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [var.public_subnet_cidr]
+  depends_on           = [azurerm_virtual_network.vnet]  # Ensure VNet is created first
 }
 
 # Private subnet masters
@@ -20,6 +21,7 @@ resource "azurerm_subnet" "master_subnet" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [var.master_subnet_cidr]
+  depends_on           = [azurerm_virtual_network.vnet]  # Ensure VNet is created first
 }
 
 # Private subnet workers
@@ -28,4 +30,5 @@ resource "azurerm_subnet" "worker_subnet" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [var.worker_subnet_cidr]
+  depends_on           = [azurerm_virtual_network.vnet]  # Ensure VNet is created first
 }
