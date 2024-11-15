b55399e27a0eb30ce6a261f53799d147f2fc3ecc
rafael.cardona.it@gmail.com <rafael.cardona.it@gmail.com>
2024-11-15 16:02:07 +0100

    done

--- ocp-terraform/05-subnets.tf
+++ ocp-terraform/05-subnets.tf
@@ -24,3 +24,19 @@ resource "azurerm_subnet" "worker_subnet" {
   address_prefixes     = [var.worker_subnet_cidr]
   depends_on           = [azurerm_virtual_network.vnet]  # Ensure VNet is created first
 }
+
+# Associate Route Tables with Subnets
+resource "azurerm_subnet_route_table_association" "public_subnet_association" {
+  subnet_id      = azurerm_subnet.public_subnet.id
+  route_table_id = azurerm_route_table.public_route_table.id
+}
+
+resource "azurerm_subnet_route_table_association" "master_subnet_association" {
+  subnet_id      = azurerm_subnet.master_subnet.id
+  route_table_id = azurerm_route_table.private_route_table.id
+}
+
+resource "azurerm_subnet_route_table_association" "worker_subnet_association" {
+  subnet_id      = azurerm_subnet.worker_subnet.id
+  route_table_id = azurerm_route_table.private_route_table.id
+}
