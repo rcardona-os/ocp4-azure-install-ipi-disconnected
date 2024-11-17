7a255195432094b78f5af7785a356f5f616645c5
rafael.cardona.it@gmail.com <rafael.cardona.it@gmail.com>
2024-11-17 02:26:19 +0100

    done

--- ocp-terraform/02-resource-group.tf
+++ ocp-terraform/02-resource-group.tf
@@ -3,3 +3,9 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
+
+# Define the Resource Group
+resource "azurerm_resource_group" "ocp_installer_rg" {
+  name     = "ocp_installer_rg"
+  location = var.location
+}
