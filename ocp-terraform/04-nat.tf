eff9986928195374b5df3169c3189369787d8014
rafael.cardona.it@gmail.com <rafael.cardona.it@gmail.com>
2024-11-17 03:23:52 +0100

    done

--- ocp-terraform/04-nat.tf
+++ ocp-terraform/04-nat.tf
@@ -3,7 +3,7 @@ resource "azurerm_nat_gateway" "master_subnet_nat_gateway" {
   name                = "master-subnet-nat-gateway"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  depends_on          = [nat_gateway_public_ip_association.id]  # Ensure public IP is created first
+  depends_on          = [azurerm_nat_gateway_public_ip_association.nat_gateway_public_ip_association] # Ensure public IP is associated first
   idle_timeout_in_minutes = 10
 }
 
