b9039a48e0a04a369f26d97311e39518744358be
rafael.cardona.it@gmail.com <rafael.cardona.it@gmail.com>
2024-11-17 02:15:12 +0100

    done

--- ocp-terraform/04-route-tables.tf
+++ ocp-terraform/04-route-tables.tf
@@ -5,9 +5,9 @@ resource "azurerm_route_table" "public_route_table" {
   location            = azurerm_resource_group.rg.location
 
   route {
-    name                   = "AllowInternetAccess"
-    address_prefix         = "0.0.0.0/0"
-    next_hop_type          = "Internet"
+    name              = "AllowInternetAccess"
+    address_prefix    = "0.0.0.0/0"
+    next_hop_type     = "Internet"
   }
 }
 
@@ -18,9 +18,9 @@ resource "azurerm_route_table" "private_route_table" {
   location            = azurerm_resource_group.rg.location
 
   route {
-    name                   = "AllowVNetAccess"
-    address_prefix         = var.vnet_cidr
-    next_hop_type          = "VnetLocal"
+    name            = "AllowVNetAccess"
+    address_prefix  = var.vnet_cidr
+    next_hop_type   = "VnetLocal"
   }
 
   # Allowing access to IP for management.azure.com
@@ -28,4 +28,5 @@ resource "azurerm_route_table" "private_route_table" {
     name           = "AllowManagementAzure"
     address_prefix = var.management_azure_ip  # Replace with actual IP for management.azure.com
     next_hop_type  = "Internet"
+  }
 }
