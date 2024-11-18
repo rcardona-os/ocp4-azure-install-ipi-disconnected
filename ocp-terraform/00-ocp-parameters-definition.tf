c8efdc6a1e2fe4b4ffe6ac8722f9debd569f9aa0
rafael.cardona.it@gmail.com <rafael.cardona.it@gmail.com>
2024-11-19 00:18:23 +0100

    done

--- ocp-terraform/00-ocp-parameters-definition.tf
+++ ocp-terraform/00-ocp-parameters-definition.tf
@@ -1,4 +1,4 @@
-# variables.tf
+# Define the Azure region where resources will be deployed.
 
 variable "location" {
   description = "The Azure region to deploy resources"
@@ -6,59 +6,69 @@ variable "location" {
   default     = "East US"
 }
 
+# Specify the Azure subscription ID where the deployment will occur.
 variable "subscription_id" {
   description = "The subscription to use for installation"
   type        = string
 }
 
+# Specify the name of the resource group in the Azure subscription.
 variable "resource_group_name" {
-  description = "The subscription to use for installation"
+  description = "The name of the resource group to use for installation"
   type        = string
 }
 
+# Provide the name of the existing Virtual Network (VNet) for infrastructure services in the OpenShift installation.
 variable "vnet_infra" {
-  description = "Existing VNet that hosts the infras services  for OCP installation"
+  description = "Existing VNet that hosts the infrastructure services for OCP installation"
   type        = string
 }
 
+# Define the private domain name used for OpenShift Private DNS configuration.
 variable "private_domain" {
   default     = "ocp-private.com"
-  description = "The subscription to use for installation"
+  description = "Private domain name for the OpenShift installation"
   type        = string
 }
 
+# Specify the management IP address or CIDR block for Azure resources.
 variable "management_azure_ip" {
   default     = "ocp-private.com"
   description = "4.150.240.10/32"
   type        = string
 }
 
+# Define the CIDR block for the Virtual Network (VNet) used in this configuration.
 variable "vnet_cidr" {
   description = "The CIDR block for the Virtual Network"
   type        = string
   default     = "10.0.0.0/16"
 }
 
+# Specify the CIDR block for the public subnet within the Virtual Network.
 variable "public_subnet_cidr" {
-  description = "The CIDR block for the Virtual Network"
+  description = "The CIDR block for the public subnet in the Virtual Network"
   type        = string
-  default     = "10.0.0.0/16"
+  default     = "10.0.0.0/24"
 }
 
+# Specify the CIDR block for the master subnet within the Virtual Network.
 variable "master_subnet_cidr" {
-  description = "The CIDR block for the Virtual Network"
+  description = "The CIDR block for the master subnet in the Virtual Network"
   type        = string
-  default     = "10.0.0.0/16"
+  default     = "10.0.1.0/24"
 }
 
+# Specify the CIDR block for the worker subnet within the Virtual Network.
 variable "worker_subnet_cidr" {
-  description = "The CIDR block for the Virtual Network"
+  description = "The CIDR block for the worker subnet in the Virtual Network"
   type        = string
-  default     = "10.0.0.0/16"
+  default     = "10.0.2.0/24"
 }
 
+# Provide the local path to the SSH public key used to access virtual machines.
 variable "ssh_key_path" {
-  description = "Local path to the ssh key used to access vms"
+  description = "Local path to the SSH public key used to access VMs"
   type        = string
   default     = "~/.ssh/id_rsa.pub"
 }
